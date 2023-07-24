5fea46ed7b608e221aaa9359947237db69923164
Kristaps Lopatko <kristaps.lopatko@gmail.com>
2023-07-24 21:41:40 +0300

    testing ASGI fix

--- ZiniVaiMini/asgi.py
+++ ZiniVaiMini/asgi.py
@@ -9,15 +9,15 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
+# from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import game.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zinivaimini.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ZiniVaiMini.settings')
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    # 'http': get_asgi_application(),
     'websocket': AuthMiddlewareStack(
         URLRouter(
             game.routing.websocket_urlpatterns
