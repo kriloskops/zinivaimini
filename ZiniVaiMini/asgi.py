b63526a6298ba35d7843703e3a0724b94dc3fa53
Kristaps Lopatko <kristaps.lopatko@gmail.com>
2023-07-25 12:40:01 +0300

    error fix

--- ZiniVaiMini/asgi.py
+++ ZiniVaiMini/asgi.py
@@ -1,26 +1,8 @@
-"""
-ASGI config for zinivaimini project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
-"""
-
 import os
+import django
+from decouple import config
+from channels.routing import get_default_application
 
-# from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import game.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ZiniVaiMini.settings')
-
-application = ProtocolTypeRouter({
-    # 'http': get_asgi_application(),
-    'websocket': AuthMiddlewareStack(
-        URLRouter(
-            game.routing.websocket_urlpatterns
-        )
-    )
-})
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", f'{config("PROJECT_NAME")}.settings')
+django.setup()
+application = get_default_application()
