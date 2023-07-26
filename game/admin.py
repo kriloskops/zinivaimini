da2d965ad775954da3925d684b4b08a4fd00d466
Kristaps Lopatko <kristaps.lopatko@gmail.com>
2023-07-26 09:13:38 +0300

    bug fix

--- game/admin.py
+++ game/admin.py
@@ -4,5 +4,11 @@ from django.contrib import admin
 from .models import Player, Question, Reply
 
 admin.site.register(Player) 
-admin.site.register(Question) 
+#admin.site.register(Question) 
 admin.site.register(Reply) 
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question', "day", "order_num")
+    ordering = ("-day", "-order_num")
